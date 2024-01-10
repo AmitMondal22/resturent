3cb1543cb536199e97f528649cd30501672b51c4
meettan <meettan@synergicsoftek.com>
2024-01-10 21:13:08 +0530

    cv sd

--- app/Http/Controllers/user/Report.php
+++ app/Http/Controllers/user/Report.php
@@ -29,7 +29,7 @@ class Report extends ResController
                 ->select('td_billing.*','md_catagory.catagory_name', 'md_food.food_name', 'md_unit_mastar.unit_name', 'users.name as customer_name', DB::raw('SUM(td_billing.total_price) as total_price_sum'))
                 ->where('td_billing.resturent_id', auth()->user()->resturent_dtls)
                 ->whereBetween('td_billing.created_at', [$r->from_date, $r->to_date])
-                ->groupBy('td_billing.billing_id','td_billing.total_price',"td_billing.id","td_billing.customer_id","td_billing.food_id","td_billing.catagory_id","td_billing.price","td_billing.qty","td_billing.resturent_id","td_billing.create_by","td_billing.created_at","td_billing.updated_at","td_billing.deleted_at","md_catagory.catagory_name", "md_food.food_name", "md_unit_mastar.unit_name", "users.name")
+                ->groupBy('td_billing.billing_id','td_billing.total_price',"td_billing.id","td_billing.customer_id","td_billing.food_id","td_billing.catagory_id","td_billing.price","td_billing.qty","td_billing.resturent_id","td_billing.create_by","td_billing.created_at","td_billing.updated_at"")
                 ->get();
             return $this->sendResponse($result, "billing report");
         /*} catch (\Throwable $th) {
